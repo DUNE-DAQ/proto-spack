4908385994e895b1fd9c5f2db238e4c69e07fc22
Spack DUNE <spacknp@epdtdi-spack-build01.cern.ch>
2021-08-03 16:00:49 +0200

    cleaning files and improving spack-build code

--- spack-build.sh
+++ spack-build.sh
@@ -2,7 +2,7 @@ export HOME=/home/spacknp
 # cloning the spack repository 0.16
 git clone https://github.com/spack/spack.git
 # cloning the new dunedaq-spack repository
-#git clone https://github.com/DUNE-DAQ/proto-spack.git
+git clone https://github.com/DUNE-DAQ/proto-spack.git
 # source the spack built-in configuration
 source $HOME/spack/share/spack/setup-env.sh
 # just test it
@@ -12,10 +12,12 @@ cd proto-spack
 spack repo add dune-build
 cd $HOME
 spack install gcc@8.2.0+binutils
-cp compilers.yaml /home/spacknp/.spack/linux/
-rm compilers-yaml
+
+spack load gcc@8.2.0
+spack compiler find
+spack compiler remove gcc@4.8.5
 cp binutils-setup.sh /home/spacknp/spack/opt/spack/linux-centos7-haswell/gcc-4.8.5/binutils-2.36.1-wozob7j2dmpqe2swmdnsltwiht2plxqe/
-rm binutils-setup.sh
+
 source compiler-setup.sh
 which gcc
 spack compilers
