62b20ee89a9941cbfd72af090fa92733e94daeb4
Spack DUNE <spacknp@epdtdi-spack-build03.cern.ch>
2021-09-08 23:52:52 +0200

    JCF: in daq-cmake_package.py I've saved the (successful) contents of spack edit daq-cmake from epdtdi-spack-build03.cern.ch

---
+++ daq-cmake_package.py
@@ -0,0 +1,40 @@
+# Copyright 2013-2021 Lawrence Livermore National Security, LLC and other
+# Spack Project Developers. See the top-level COPYRIGHT file for details.
+#
+# SPDX-License-Identifier: (Apache-2.0 OR MIT)
+
+# ----------------------------------------------------------------------------
+# If you submit this package back to Spack as a pull request,
+# please first remove this boilerplate and all FIXME comments.
+#
+# This is a template package file for Spack.  We've put "FIXME"
+# next to all the things you'll want to change. Once you've handled
+# them, you can save this file and test your package like this:
+#
+#     spack install daq-cmake
+#
+# You can edit this file again by typing:
+#
+#     spack edit daq-cmake
+#
+# See the Spack documentation for more information on packaging.
+# ----------------------------------------------------------------------------
+
+from spack import *
+
+
+class DaqCmake(CMakePackage):
+    """CMake support for DUNE-DAQ packages"""
+
+    homepage = "https://github.com/DUNE-DAQ/daq-cmake"
+    url =      "https://github.com/DUNE-DAQ/daq-cmake"
+
+    maintainers = ['jcfreeman2']
+
+    version('master', branch='master', git=homepage)
+
+    #generator = 'Ninja'
+
+    # FIXME: Add dependencies if required.
+    #depends_on('ninja', type='build')
+
