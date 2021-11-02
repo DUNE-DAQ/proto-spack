92e299d6e5752e94c1fa589cedb7cb7337b4a75d
Spack DUNE <spacknp@epdtdi-spack-build03.cern.ch>
2021-11-02 17:28:48 +0100

    JCF: various improvements to generate_new_package.sh
    Specifically:
    -Use dbt-settings.sh to deduce the package specific version given the suit version
    -Bake in a dependency to daq-cmake and py-moo
    -Ensure that <PACKAGE>_SHARE environment variable is set
    -Ensure that PYTHONPATH gets added to

--- generate_new_package.sh
+++ generate_new_package.sh
@@ -37,6 +37,15 @@ if [[ ! -e ${packagefile}.backup ]]; then
     cp -p $packagefile{,.backup}
 fi
 
+tmpdir=$(mktemp -d)
+cd $tmpdir 
+curl -O https://raw.githubusercontent.com/DUNE-DAQ/daq-release/develop/configs/$version/dbt-settings.sh
+pkgversion=$( sed -r -n 's/_/./g;s/.*"'$name'\s+v([0-9.]+).*/\1/Ip' dbt-settings.sh )
+cd -
+if [[ "$tmpdir" =~ ^/tmp ]]; then
+    rm -rf $tmpdir
+fi
+
 # Get rid of the boilerplate between the "-"'s in the header comment
 sed -i -r '/^# -/,/^# -/d' $packagefile
 
@@ -49,8 +58,8 @@ sed -i -r 's!homepage = ".*"!homepage = "https://dune-daq-sw.readthedocs.io/en/'
 # Yours truly will maintain the Spack package
 sed -i -r 's!^(\s*)#\s*maintainers.*!\1maintainers = ["jcfreeman2"]!' $packagefile
 
-# Switch the auto-generated version from X.Y.Z to whatever was specified (e.g. dunedaq-vX.Y.Z)
-sed -i -r 's/version\([^,]+,/version\("'$version'",/' $packagefile
+# Switch the auto-generated version from the suite release version to the per-package version
+sed -i -r 's/version\([^,]+,/version\("'$pkgversion'",/' $packagefile
 
 # And since the new version probably means Spack can't auto-deduce the tarball, tell it explicitly what it is 
 sed -i -r '/^\s*version/s!\)s*$!, extension="tar.gz", url="'$url'"\)!' $packagefile
@@ -59,9 +68,15 @@ echo "Please make your edits (e.g., add dependencies, remove FIXMEs, etc) by run
 
 cat<<EF >> $packagefile
 
+    depends_on("daq-cmake")
+    depends_on('py-moo', type='build')
+
     def setup_run_environment(self, env):
-        env.prepend_path("DUNEDAQ_SHARE_PATH", self.prefix + "/share")
+        env.set(self.__module__.split(".")[-1].upper().replace("-", "_") + "_SHARE", self.prefix + "/share" )
+        env.prepend_path('DUNEDAQ_SHARE_PATH', self.prefix + "/share")
         env.prepend_path('CET_PLUGIN_PATH', self.prefix.lib + "64")
+        env.prepend_path("PYTHONPATH", self.prefix.lib + "64/python")
+
 
 EF
 
