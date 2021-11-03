2163e8ff406260c0258b20ddea9ae2e5fa61b55d
Spack DUNE <spacknp@epdtdi-spack-build03.cern.ch>
2021-11-03 18:18:47 +0100

    JCF: remove cruft from pistache's package.py

--- dune-build/packages/pistache/package.py
+++ dune-build/packages/pistache/package.py
@@ -42,20 +42,6 @@ class Pistache(CMakePackage):
         copy(join_path(os.path.dirname(__file__),
              "PistacheTargets-release.cmake"), self.prefix + "/PistacheTargets-release.cmake")
 
-        print(f"IN THE PATCH, cwd == {os.getcwd()}")
-        os.system("find . -name \"*.so\"")
-        print("Just looked for shared object libraries")
-      
-    def setup_build_environment(self, env):
-        env.prepend_path("DUNEDAQ_SHARE_PATH", self.prefix + "/share")
-        env.prepend_path('CET_PLUGIN_PATH', self.prefix.lib + "64")
-
     def install(self, spec, prefix):
         super().install(spec, prefix)
-
-        print([f for f in dir(CMakePackage) if not f.startswith('_')])
-        print(self.build_directory)
         os.system(f"cp -p {self.build_directory}/src/*.so* {self.prefix}/lib64")
-        #print(f"cp -rp {self.stage.source_path}/include {self.prefix}/include")
-        #os.system(f"cp -rp {self.stage.source_path}/include {self.prefix}/include")
-        #print(f"source_path is {self.stage.source_path}")
