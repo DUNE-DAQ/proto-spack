12177efbc4b131ca19c96107d64c63a6fd6d0800
Spack DUNE <spacknp@epdtdi-spack-build02.cern.ch>
2022-02-02 17:38:32 +0100

    JCF: be able to control whether or not systems depends on a debug build of Python. What I've discovered is that you need a debug build of Python to build gdb+python, but a non-debug build of Python for dbt-create-pyvenv.sh to work

--- dune-build/packages/systems/package.py
+++ dune-build/packages/systems/package.py
@@ -16,7 +16,11 @@ class Systems(BundlePackage):
     version("dunedaq-v2.9.0")
     version("dunedaq-v2.8.2")
 
+    variant('debug', default=False,
+            description='Switch to the debug version of Python')
+
     for ver in ["develop", "dunedaq-v2.8.2", "dunedaq-v2.9.0"]:
-        depends_on("python@3.8.3 +debug", when=f"@{ver}") # +debug is for gdb
+        depends_on("python@3.8.3 +debug", when=f"@{ver} +debug") # +debug is so gdb builds with Python support
+        depends_on("python@3.8.3 ~debug", when=f"@{ver} ~debug") # ~debug is for dbt-create-pyvenv.sh in daq-buildtools
         depends_on("gcc@8.2.0 +binutils", when=f"@{ver}")
         
