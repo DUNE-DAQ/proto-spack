0dbfd19d3702307af9527c2c2bb9e7113cc54992
John Christian Freeman <jofreema@np04-srv-019.cern.ch>
2022-03-08 21:14:52 +0100

    JCF: get rid of that annoying prompt when installing trace where Spack asks you to choose CMakeLists.txt or CMakeLists.txt.old when patching

--- dune-build/packages/trace/package.py
+++ dune-build/packages/trace/package.py
@@ -19,7 +19,7 @@ class Trace(CMakePackage):
     version('3.16.02', commit='570d91139f7277966ae5259b0a11d85f8574e5dc')   # JCF, Sep-9-2021: v3_16_02 is used in the dunedaq-v2.8.0 suite
     version('3.15.09', commit='f429a6a8b52925c31678cab5643f67df16f06fd5')
 
-    patch('disable_cetmodules.diff')
+    patch('disable_cetmodules.diff', sha256='640816dfe077382bdb5345c8b5568c92e1ccb36274887e57241d826522ba5686')
     patch('install-exec.diff', when='3.15.09')
     patch('install-scripts.diff', when='stable')
 
